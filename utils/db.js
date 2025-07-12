e19efe218dc98d04444a04aa2984a25a17c9a9f9
kastigator <alexandr_lubimov@icloud.com>
2025-07-12 17:32:48 +0300

    изменил db env cloudbuld. из-за необходимого соединения с cloud run и cloud sql

--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,23 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT || 3306,
+const config = {
   user: process.env.DB_USER || 'kastigator',
   password: process.env.DB_PASSWORD || '192168',
   database: process.env.DB_NAME || 'crusher_parts_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
+};
+
+// Подключение через сокет, если указан DB_HOST как /cloudsql/...
+if (process.env.DB_HOST && process.env.DB_HOST.startsWith('/cloudsql/')) {
+  config.socketPath = process.env.DB_HOST;
+} else {
+  config.host = process.env.DB_HOST || '127.0.0.1';
+  config.port = process.env.DB_PORT || 3306;
+}
+
+const pool = mysql.createPool(config);
 
 module.exports = pool;
